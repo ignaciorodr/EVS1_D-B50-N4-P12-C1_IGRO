040ecfb3bca2a3ed0dfda2140ffd75eed6cb9e96
Nacho <ignacio.rodriguez.jimenez19@gmail.com>
2022-09-22 09:51:03 -0500

    app listo 2

--- igro_proyecto/urls.py
+++ igro_proyecto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App2 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('segunda/', views.display)
 ]
